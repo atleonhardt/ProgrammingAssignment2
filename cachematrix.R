f0221bf8c9e1f6a65ca0bb42f7d64b5087dc8525
atleonhardt <atleonhardt@gmail.com>
2014-08-09 01:02:43 +0200

    modified matrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Similar to the makeVector function, here we "add" functions to our special 
+#matrix.  We will later call on these functions so that we don't have to
+#repeat calculations.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+#As in the example we define the get and set functions, and assign the solve 
+#function.
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ makeCacheMatrix <- function(x = numeric()) {
+     m <- NULL
+     set <- function(y) {
+         x <<- y
+         m <<- NULL
+     }
+     get <- function() x
+     setsolve<-function(solve) m <<- solve
+     getsolve<- function() m
+     list(set = set, get = get,
+          setsolve=setsolve,
+          getsolve=getsolve)
+ }
+ 
+#The cacheSolve allows us to either perform the calculation or recall it from 
+#memory in case it has already been calculated.
+
+ cacheSolve <- function(x,...) {
+     m <- x$getsolve()
+     if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+     }
+     data <- x$get()
+     m <- solve(data,...)
+     x$setsolve(m)
+     m
+ }
